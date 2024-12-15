eebecceadface9576af44cde4da7fabc5d51c94d
lauespinosa <laura.espinosa@ecdc.europa.eu>
2024-12-15 21:19:02 +0100

    start repo

---
+++ README.md
@@ -0,0 +1,9 @@
+# LLM benchmarking for public health
+
+This repository contains the code and data for LLM benchmarking for public health.
+
+The following files and folders are available:
+
+* **R project:** to facilitate the working directory and reproducibility of the code.
+* **Data:** this folder has data used in this project. 
+* **Scripts:** this folder contains the scripts used.
---
+++ script/0_main.R
@@ -0,0 +1,16 @@
+#' Author: Laura Espinosa
+#' Date created: 15 December 2024
+#' Date updated: 15 December 2024
+
+
+# Packages ----------------------
+## install/load "pacman" to help installing and loading other packages
+message("Installing and loading packages")
+
+while (require("pacman") == FALSE) {
+  install.packages("pacman")
+  library("pacman")
+}
+
+## load packages
+p_load(tidyverse, vroom)

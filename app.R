eb89af2bdae4566e23f4381ef1e2a34afe0e5451
lauespinosa <laura.espinosa@ecdc.europa.eu>
2025-01-25 15:46:33 +0100

    More LLM evaluation results and improved Shiny

--- app.R
+++ app.R
@@ -2,10 +2,23 @@ library(shiny)
 library(tidyverse)
 library(jsonlite)
 library(caret)
-library(DT) # For formatted tables
+library(DT)
+library(shinyBS)
 
 # Define UI for the application
-ui <- fluidPage(
+ui <- navbarPage(
+  title = "LLM benchmarking tool",
+  tabPanel(
+    title = "Instructions",
+    fluidPage(
+      sidebarPanel(
+      p = "This is the tab with instructions"
+    )
+    )
+  ),
+  tabPanel(
+    title = "Evaluation of categorial variables",
+  fluidPage(
   titlePanel("Evaluation Pipeline"),
   sidebarLayout(
     sidebarPanel(
@@ -13,14 +26,27 @@ ui <- fluidPage(
       h4("Setting up variables and paths"),
       p("Specify the required file paths, LLM details, target columns, and experiment name below."),
       textInput("dataset", "Dataset file path", "data/datasets/epfl_1000_clean_data_no_text.csv"),
-      textInput("llm_results", "LLM results file path", "data/llm_results/llm_tweets_en_epfl.json"),
+      div(
+        style = "display: flex; align-items: center;",
+        checkboxInput("apply_transform", "Data transformation for LLM results", value = FALSE),
+        actionButton("help_transform", "?", class = "btn-sm")
+      ),
+      
+      bsTooltip("help_transform", 
+                title = "When selected, the JSON LLM results file will undergo transformations such as unnesting and column renaming.   ", 
+                placement = "right", 
+                trigger = "hover"),
+      textInput("llm_results", "LLM results file path", "data/llm_results/llm_tweets_en_epfl_gpt4o.csv"),
       textInput("evaluation_path", "Evaluation results path", "data/evaluation/"),  # New input for evaluation directory
       textInput("llm", "LLM", "gpt4o"),
       textInput("target", "Target column", "stance_target"),
       textInput("llm_prediction", "LLM prediction column", "stance"),
       textInput("experiment", "Experiment", "tweet_epfl_en"),
       
+      actionButton("refresh_code", "Refresh settings"),
+      
       h4("Pipeline steps"),
+      p("Execute each step sequentially. Results and messages will appear in the right panel."),
       fluidRow(
         column(6, 
                actionButton("import_dataset", "1. Import dataset", style = "display: block; width: 100%; margin-bottom: 10px;"),
@@ -33,8 +59,17 @@ ui <- fluidPage(
                actionButton("view_accuracy", "6. View accuracy", style = "display: block; width: 100%; margin-bottom: 10px;")
         )
       ),
-      hr(),
-      p("Execute each step sequentially. Results and messages will appear in the right panel.")
+      fluidRow(
+        column(12,  # Occupies the entire width
+               actionButton("llm_bench_stats", "7. LLM benchmarking (import all confusion matrix stats results)", style = "display: block; width: 100%; margin-bottom: 10px; font-weight: bold;")
+        )
+      ),
+      fluidRow(
+        column(12,  # Occupies the entire width
+               actionButton("llm_bench_accuracy", "8. LLM benchmarking (import all accuracy results)", style = "display: block; width: 100%; margin-bottom: 10px; font-weight: bold;")
+        )
+      ),
+      hr()
     ),
     mainPanel(
       h4("Pipeline Results"),
@@ -42,11 +77,69 @@ ui <- fluidPage(
     )
   )
 )
+),
+tabPanel(
+  title = "Evaluation of numerical variables",
+  fluidPage(
+    titlePanel("Evaluation Pipeline"),
+    sidebarLayout(
+      sidebarPanel(
+        tags$style(HTML("h4 { font-weight: bold; }")),
+        h4("Setting up variables and paths"),
+        p("Specify the required file paths, LLM details, target columns, and experiment name below."),
+        textInput("dataset_num", "Dataset file path", "data/datasets/epfl_1000_clean_data_no_text.csv"),
+        textInput("llm_results_num", "LLM results file path", "data/llm_results/llm_tweets_en_epfl.json"),
+        textInput("evaluation_path_num", "Evaluation results path", "data/evaluation/"),  # New input for evaluation directory
+        textInput("llm_num", "LLM", "gpt4o"),
+        textInput("target_num", "Target column", "stance_target"),
+        textInput("llm_prediction_num", "LLM prediction column", "stance"),
+        textInput("experiment_num", "Experiment", "tweet_epfl_en"),
+        
+        h4("Pipeline steps"),
+        p("Execute each step sequentially. Results and messages will appear in the right panel."),
+        fluidRow(
+          column(6, 
+                 actionButton("import_dataset_num", "1. Import dataset", style = "display: block; width: 100%; margin-bottom: 10px;"),
+                 actionButton("join_data_num", "3. Join target & LLM results", style = "display: block; width: 100%; margin-bottom: 10px;"),
+                 actionButton("view_stats_num", "5. View confusion matrix stats", style = "display: block; width: 100%; margin-bottom: 10px;")
+          ),
+          column(6, 
+                 actionButton("import_llm_num", "2. Import LLM results", style = "display: block; width: 100%; margin-bottom: 10px;"),
+                 actionButton("calc_conf_matrix_num", "4. Calculate confusion matrix", style = "display: block; width: 100%; margin-bottom: 10px;"),
+                 actionButton("view_accuracy_num", "6. View accuracy", style = "display: block; width: 100%; margin-bottom: 10px;")
+          )
+        ),
+        fluidRow(
+          column(12,  # Occupies the entire width
+                 actionButton("step_7", "7. LLM benchmarking (import all evaluation results)", style = "display: block; width: 100%; margin-bottom: 10px; background-color: #f0f0f0; font-weight: bold;")
+          )
+        ),
+        hr()
+      ),
+      mainPanel(
+        h4("Pipeline Results"),
+        uiOutput("results_num") # Dynamic output for either messages or tables
+      )
+    )
+  )
+)
+)
 
 # Define server logic
 server <- function(input, output, session) {
+  dataset_path <- reactive({
+    req(input$dataset)  # Ensure the input is not empty or NULL
+    input$dataset       # Return the dataset path
+  })
+  
+  llm_results_path <- reactive({
+    req(input$llm_results)  # Ensure the input is not empty or NULL
+    input$llm_results       # Return the LLM results path
+  })
+
   # Reactive values to store intermediate results
   values <- reactiveValues(
+    dataset = NULL,
     df_target = NULL,
     df_llm_results = NULL,
     df_target_llm = NULL,
@@ -55,10 +148,23 @@ server <- function(input, output, session) {
     accuracy = NULL
   )
   
+  # Refresh settings
+  observeEvent(input$refresh_code, {
+    values$dataset <- NULL
+    values$df_target <- NULL
+    values$df_llm_results <- NULL
+    values$df_target_llm <- NULL
+    
+    output$results <- renderUI({
+      h5("Settings refreshed. Ready for new inputs.")
+    })
+  })
+  
   # Import dataset
   observeEvent(input$import_dataset, {
+    #dataset <- input$dataset
     tryCatch({
-      values$df_target <- read_csv(input$dataset)
+      values$df_target <- read_csv(dataset_path())
       output$results <- renderUI({
         h5("Dataset successfully imported.")
       })
@@ -71,21 +177,48 @@ server <- function(input, output, session) {
   
   # Import LLM results
   observeEvent(input$import_llm, {
+    #df_llm_results <- input$llm_results
     tryCatch({
-      df_llm_json <- fromJSON(input$llm_results)
-      values$df_llm_results <- df_llm_json$annotations %>%
-        unlist() %>%
-        as.data.frame() %>%
-        rownames_to_column(var = "id") %>%
-        rename(variables = ".") %>%
-        mutate(id = str_extract(id, "\\d+"),
-               variables = map(variables, ~ fromJSON(.x))) %>%
-        unnest_wider(variables) %>%
-        mutate(across(everything(), ~ tolower(as.character(.))),
-               id = as.numeric(id))
-      output$results <- renderUI({
-        h5("LLM results successfully imported.")
-      })
+      llm_file <- llm_results_path()
+      file_ext <- tools::file_ext(llm_file)  # Get the file extension (e.g., "csv", "json")
+      
+      if (file_ext == "json") {
+        # Handle JSON files
+        df_llm <- fromJSON(llm_file)
+        
+        if (input$apply_transform) {
+          # Apply transformation for JSON
+          values$df_llm_results <- df_llm$annotations %>%
+            unlist() %>%
+            as.data.frame() %>%
+            rownames_to_column(var = "id") %>%
+            rename(variables = ".") %>%
+            mutate(id = str_extract(id, "\\d+"),
+                   variables = map(variables, ~ fromJSON(.x))) %>%
+            unnest_wider(variables) %>%
+            mutate(across(everything(), ~ tolower(as.character(.))),
+                   id = as.numeric(id))
+          
+          output$results <- renderUI({
+            h5("LLM results successfully imported and transformed (JSON).")
+          })
+        } else {
+          # No transformation, just flatten JSON
+          values$df_llm_results <- as.data.frame(df_llm)
+          output$results <- renderUI({
+            h5("LLM results successfully imported (JSON).")
+          })
+        }
+      } else if (file_ext == "csv") {
+        # Handle CSV files
+        values$df_llm_results <- read_csv(llm_file)
+        output$results <- renderUI({
+          h5("LLM results successfully imported (CSV).")
+        })
+      } else {
+        stop("Unsupported file type. Please provide a CSV or JSON file.")
+      }
+      
     }, error = function(e) {
       output$results <- renderUI({
         h5(paste("Error importing LLM results:", as.character(e)))
@@ -95,6 +228,7 @@ server <- function(input, output, session) {
   
   # Join target and LLM results
   observeEvent(input$join_data, {
+    req(input$target, input$llm_prediction)
     tryCatch({
       req(values$df_target, values$df_llm_results)
       values$df_target_llm <- values$df_target %>%
@@ -125,7 +259,7 @@ server <- function(input, output, session) {
       
       values$conf_matrix <- confusionMatrix(df_conf_matrix %>% pull(!!sym(input$llm_prediction)),
                                             df_conf_matrix %>% pull(!!sym(input$target)),
-                                            mode = "everything")
+                                            mode = "everything") 
       output$results <- renderUI({
         h5("Confusion matrix successfully calculated.")
       })
@@ -143,7 +277,12 @@ server <- function(input, output, session) {
       values$stats_conf_matrix <- values$conf_matrix$byClass %>%
         as.data.frame() %>%
         rownames_to_column() %>%
-        rename(Class = rowname)
+        rename(Class = rowname) %>% 
+        mutate(LLM = input$llm,
+               Experiment = input$experiment) %>% 
+        select(Experiment, LLM, everything()) %>% 
+        mutate(across(where(is.numeric), 
+                      ~ round(., digits = 4)))
       
       write_csv(
         values$stats_conf_matrix, 
@@ -169,7 +308,12 @@ server <- function(input, output, session) {
         as.data.frame() %>%
         rownames_to_column() %>%
         rename(!!sym(input$llm) := ".",
-               "accuracy_variable" = "rowname")
+               "accuracy_variable" = "rowname") %>% 
+        mutate(LLM = input$llm,
+               Experiment = input$experiment) %>% 
+        select(Experiment, LLM, everything()) %>% 
+        mutate(across(where(is.numeric), 
+                      ~ round(., digits = 4)))
       
       write_csv(
         values$accuracy, 
--- scripts/1_prepare_datasets.R
+++ scripts/1_prepare_datasets.R
@@ -4,8 +4,24 @@
 #' Date created: 15 December 2024
 #' Date updated: 30 December 2024
 
-# Import datasets ---------------------
+# Clean datasets ---------------------
+## WHO DONs ---------------
 df_don <- read_csv("data/datasets/DON-db_mergedTexts.csv")
+
+df_don_cleaned <- df_don %>% 
+  filter(!is.na(`texts-reduced`)) %>% 
+  filter(!is.na(CasesTotal)) %>% 
+  filter(!is.na(ISO))  %>% 
+  filter(!is.na(DiseaseLevel1)) %>% 
+  mutate(id = 1:nrow(.)) 
+
+df_don_cleaned_300 <- df_don_cleaned %>% 
+  sample_n(300) %>% 
+  rename(`00_MSG_00_TEXT` = `texts-reduced`)
+
+df_don_cleaned_300 %>% write_csv("data/datasets/DON-db_merged_300_all_variables.csv")
+
+df_don_cleaned_300 %>% select(id, `00_MSG_00_TEXT`) %>% write_csv("data/datasets/DON-db_merged_300_text.csv")
 
 df_don_text <- df_don %>% 
   filter(!is.na(`texts-reduced`)) %>% 
@@ -28,3 +44,23 @@ df_don_cases_total <- df_don %>%
   select(`texts-reduced`, CasesTotal)
 
 df_don_cases_total %>% write_csv("data/datasets/DON-db_merged_cases_total.csv")
+
+## Facebook posts ----------------
+df_fb_anti <- read_csv("data/datasets/local/anti - anti.csv") %>% 
+  filter(!is.na(CLASS_COMMENT)) %>% 
+  sample_n(150) %>% 
+  mutate(id = 0:149)
+
+df_fb_pro <- read_csv("data/datasets/local/pro - pro.csv") %>% 
+  filter(!is.na(CLASS_COMMENT)) %>% 
+  sample_n(150) %>% 
+  mutate(id = 0:149)
+
+df_fb <- df_fb_anti %>% 
+  rbind(df_fb_pro) %>% 
+  mutate(id = 0:299) %>% 
+  rename(`00_MSG_00_TEXT` = message)
+
+df_fb %>% write_csv("data/datasets/local/facebook_anti_pro_300_comments.csv")
+
+df_fb %>% select(id, post_id, CLASS_COMMENT) %>% write_csv("data/datasets/facebook_anti_pro_300_comments_anonymised.csv")
